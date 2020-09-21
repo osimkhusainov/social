ebd0111f2455972098e910dc675612224733b2cc
Osim <57813114+osimkhusainov@users.noreply.github.com>
2020-09-21 23:56:34 +0300

    Add files via upload

---
+++ precache-manifest.d335a7178f60fb1481ad9f2ebcd10a42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5de32d99426382a81796b7b7de167d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31be25280e0496fa4d87",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "3241490a8be81e6583eb",
+    "url": "/static/css/main.7689019f.chunk.css"
+  },
+  {
+    "revision": "31be25280e0496fa4d87",
+    "url": "/static/js/2.e331bbf9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e331bbf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3241490a8be81e6583eb",
+    "url": "/static/js/main.a92a8e68.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
